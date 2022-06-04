de3301e2bf6989b1c3e4832527135e522c5e140e
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-06-04 20:46:23 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -37,7 +37,7 @@ SONG_DOWNLOAD_DURATION = int(
 )  # Remember to give value in Minutes
 
 # You'll need a Private Group ID for this.
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001584010815)
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001584010815")
 
 # A name for your Music bot.
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "@TheKaizuryu Music Bot")
