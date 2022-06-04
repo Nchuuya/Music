02c089a6c9fc5769b8e05a3a51d1e832eb6ed2a3
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-06-04 21:29:16 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001584010815")
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Boa Hancock")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "@BoaHancock_Robot")
 
 # Your User ID.
 OWNER_ID = list(
